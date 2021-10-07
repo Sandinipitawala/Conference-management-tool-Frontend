36b106c18da0b3e9de6ef8dc267e4a158520fdac
Sandini <sandinipitawala1@gmail.com>
2021-10-07 23:42:00 +0530

    added contactus

---
+++ src/component/common/contact-us.component.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import UserService from "../../service/user.service";
+import Contactus from "../../component/common/contactus-form.component"; 
+
+export default class ContactUs extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: ""
+        };
+    }
+
+    componentDidMount() {
+        UserService.getPublicContentContactUs().then(
+            response => {
+                this.setState({
+                    content: response.data
+                });
+            },
+            error => {
+                this.setState({
+                    content:
+                        (error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div className="container">
+                    <Contactus/>
+            </div>
+        );
+    }
+}
+
